b36fe390fd3e6f061175259d6e86a9cdb45798a6
Kevin Partington <platinum.azure@kernelpanicstudios.com>
2016-07-22 09:18:52 +0000

    Adding stub JavaScript functions

--- scripts/memory.js
+++ scripts/memory.js
@@ -15,6 +15,22 @@ $(function () {
             $("#tiles").append($tileRow);
         }
     }
+    
+    function shuffle() {
+      
+    }
+
+    function assignPokemonToTile() {
+    }
+    
+    function checkMatch() {
+    }
+    
+    function onClickTile() {
+    }
+    
+    function reset() {
+    }
 
     $("#tiles").empty();
 
