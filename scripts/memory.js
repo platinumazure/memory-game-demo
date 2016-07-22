8952d66be192fd96f91fba0c015eb4710b7ccbec
Kevin Partington <platinum.azure@kernelpanicstudios.com>
2016-07-22 10:01:21 +0000

    Ensuring tiles have an index

--- scripts/memory.js
+++ scripts/memory.js
@@ -4,11 +4,17 @@ $(function () {
     const gridSize = 4;
 
     function renderTiles() {
+        let index = 0;
+        
         for (let row = 0; row < gridSize; row += 1) {
             const $tileRow = $("<div class='tile-row'></div>");
 
             for (let column = 0; column < gridSize; column += 1) {
                 const $tile = $("<div class='tile'></div>");
+                
+                $tile.data("tileIndex", index);
+                index += 1;
+                
                 $tileRow.append($tile);
             }
 
